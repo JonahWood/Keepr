7f6aea5d7e5cac3d8fbf982b779279abb6fcdbb8
Jonah <jonah.osito@icloud.com>
2023-03-31 10:52:32 -0600

    basicssss

--- Keepr/dbSetup.sql
+++ Keepr/dbSetup.sql
@@ -6,3 +6,17 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+
+CREATE TABLE IF NOT EXISTS keeps(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  creatorId VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(1000),
+  img VARCHAR(1000) NOT NULL,
+  views INT NOT NULL DEFAULT 0,
+  kept INT,
+
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+
+DROP TABLE keeps;
