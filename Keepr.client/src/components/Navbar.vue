45f2432c27b1f363665ab385c99b2b27fe2cfdc6
Jonah <jonah.osito@icloud.com>
2023-04-01 17:42:56 -0600

    some set up style stuffsss

--- Keepr.client/src/components/Navbar.vue
+++ Keepr.client/src/components/Navbar.vue
@@ -1,26 +1,21 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+  <nav class="navbar navbar-expand-lg navbar-dark px-3 kHeader">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+        <img alt="logo"
+          src="https://static.vecteezy.com/system/resources/previews/016/316/977/original/gold-text-effect-letter-k-3d-render-png.png"
+          height="45" />
       </div>
     </router-link>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarText"
-      aria-controls="navbarText"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
         <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
+          <router-link :to="{ name: 'Home' }" class="btn home-button selectable text-uppercase">
+            Home
           </router-link>
         </li>
       </ul>
@@ -41,6 +36,19 @@ export default {
 </script>
 
 <style scoped>
+.home-button {
+  background-color: #E9D8D6;
+  border-bottom: 0px solid black !important;
+  border-radius: 40%;
+  border-bottom-left-radius: 40% !important;
+  border-bottom-right-radius: 40% !important;
+  font-family: 'Oxygen', sans-serif;
+}
+
+.kHeader {
+  border-bottom: 2px solid #E9D8D6;
+}
+
 a:hover {
   text-decoration: none;
 }
@@ -60,5 +68,4 @@ a:hover {
     height: 64px;
   }
 }
-
 </style>
--- Keepr/Controllers/ProfilesController.cs
+++ Keepr/Controllers/ProfilesController.cs
@@ -1,20 +1,20 @@
-namespace Keepr.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProfilesController : ControllerBase
-    {
-        [HttpGet("{id}")]
-        public ActionResult<Profile> GetProfile(int id)
-        {
-            try 
-            {
+// namespace Keepr.Controllers
+// {
+//     [ApiController]
+//     [Route("api/[controller]")]
+//     public class ProfilesController : ControllerBase
+//     {
+//         [HttpGet("{id}")]
+//         public ActionResult<Profile> GetProfile(int id)
+//         {
+//             try 
+//             {
             
-            }
-            catch (Exception e)
-            {
-            return BadRequest(e.Message);
-            }
-        }
-    }
-}
+//             }
+//             catch (Exception e)
+//             {
+//             return BadRequest(e.Message);
+//             }
+//         }
+//     }
+// }
